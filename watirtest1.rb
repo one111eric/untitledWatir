58e33646d2504eb06a7b307c64f8cdcbcfb1b7bc
one111eric <one111eric@hotmail.com>
2015-05-28 18:40:52 -0700

    Initial commit

---
+++ watirtest1.rb
@@ -0,0 +1,14 @@
+require 'watir-webdriver'
+class Watirtest1
+def open_page
+  puts "abc"
+  browser=Watir::Browser.new :ff
+  browser.goto("google.com")
+  browser.text_field(:name=>"q").set("San Francisco")
+  browser.send_keys :return
+  browser.close
+end
+
+end
+  x=Watirtest1.new
+  x.open_page
